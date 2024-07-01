6bbe279db8fce1411b9738e14b4adafe4d732f8a
Nadir Alcalde <nadir90370@gmail.com>
2024-07-02 00:09:27 +0200

    main has now all necessary commands

--- main.py
+++ main.py
@@ -3,8 +3,12 @@ import subprocess
 
 subprocess.run(['python', 'user_preprocess.py','--input', 'sample_data/article_1.json', '--output', 'output' ])
 
-subprocess.run(['python', 'user_inference.py', '--query', 'bunu cevir', '--query_id','1', '--output', 'output' ])
-
 subprocess.run(['python', 'prepare_dataset_not_tokenized.py'])
 
 subprocess.run(['python', 'fine_tune_model.py'])
+
+subprocess.run(['python', 'evaluate_model.py'])
+
+subprocess.run(['python', 'user_inference.py', '--query', 'your input in es, ar, tur', '--query_id','1', '--output', 'output' ])
+
+
